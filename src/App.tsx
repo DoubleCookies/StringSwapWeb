de9f624631c9e02840e062fc7ba419f41922a780
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2021-07-15 14:13:50 +0300

    Readme, better placeholder, remove some things

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
+    <div className="app">
+      <header className="app-header">
+        <div>
           Work in progress ༼ つ ◕_◕ ༽つ
-        </p>
+        </div>
       </header>
+      <main className="main-content">
+
+      </main>
+      <footer className="footer-block">
+        <div>2021 <span role="img" aria-label="cookieEmoji">🍪</span></div>
+      </footer>
     </div>
   );
 }
