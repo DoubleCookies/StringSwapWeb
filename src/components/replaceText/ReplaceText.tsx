229e7f5df801d053ca241d785870e0afcb7fcbea
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-01-21 12:24:35 +0300

    code format

--- src/components/replaceText/ReplaceText.tsx
+++ src/components/replaceText/ReplaceText.tsx
@@ -34,7 +34,7 @@ class ReplaceText extends React.Component<MyProps, MyState> {
   }
 
   replaceText() {
-    const {startValue, replaceStart, replaceFinish} = this.state;
+    const { startValue, replaceStart, replaceFinish } = this.state;
     if (replaceStart) {
       const resultString = startValue.replaceAll(replaceStart, replaceFinish);
       this.setState({
@@ -69,27 +69,34 @@ class ReplaceText extends React.Component<MyProps, MyState> {
   render() {
     return (
       <form>
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-          <div style={{flexDirection: 'column', flexBasis: '50%'}}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div style={{ flexDirection: 'column', flexBasis: '50%' }}>
             <label className="form-label">
               Текст для замены:
             </label>
-            <textarea className="form-text-area" placeholder="Введите текст для замены" onChange={this.updateStartValue.bind(this)} rows={10}/>
-            <div style={{margin: 10}}>
+            <textarea className="form-text-area"
+                      placeholder="Введите текст для замены"
+                      onChange={this.updateStartValue.bind(this)}
+                      rows={10}/>
+            <div style={{ margin: 10 }}>
               <label>Что</label>
-              <input className="process-button" onChange={this.handleReplaceStart.bind(this)}
-                     type="text" value={this.state.replaceStart}/>
+              <input className="process-button"
+                     onChange={this.handleReplaceStart.bind(this)}
+                     type="text"
+                     value={this.state.replaceStart}/>
               <br/>
               <label>На что</label>
-              <input className="process-button" onChange={this.handleReplaceFinish.bind(this)}
-                     type="text" value={this.state.replaceFinish}/>
+              <input className="process-button"
+                     onChange={this.handleReplaceFinish.bind(this)}
+                     type="text"
+                     value={this.state.replaceFinish}/>
             </div>
           </div>
-          <div style={{flexDirection: 'column', flexBasis: '50%'}}>
+          <div style={{ flexDirection: 'column', flexBasis: '50%' }}>
             <label className="form-label">
               Результат:
             </label>
-            <textarea className="form-text-area" value={this.state.finishValue} readOnly={true} rows={10} />
+            <textarea className="form-text-area" value={this.state.finishValue} readOnly={true} rows={10}/>
           </div>
         </div>
       </form>
